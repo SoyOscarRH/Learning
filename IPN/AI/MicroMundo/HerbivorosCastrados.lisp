adef258692e4f01e1493ed511bcfc16353695555
SoyOscarRH <soyoscarrh@gmail.com>
2019-11-28 21:24:18 -0600

    a

--- IPN/AI/MicroMundo/HerbivorosCastrados.lisp
+++ IPN/AI/MicroMundo/HerbivorosCastrados.lisp
@@ -1,36 +1,16 @@
 ; Start rule (create 10 herbivores)
 (defrule start
-	:group 
-    :initialization
+	:group :initialization
 	:when
-	:do
-	  (set-entities :herbivore 10 :desert))
+	:do (set-entities :herbivore 10 :desert))
 
-
-; Find food
-(defrule search-food
-  :group 
-    :herbivores
-  :when
-		(< (get-entity-food @id) 50)
-
-    (search-distant-cell @cell1
-      (equal (get-cell-type @cell1) :desert)
-      (simulate-move @cell1 (get-entity-coordinates @id) @cell1 :diagonal))
-      
-  :do 
-    (move-entity-to @id @cell1 :diagonal))
-
-
-(defrule get-foot-group
+; Get foot
+(defrule get-foot
 	:group
 		:herbivores
 	:when
-    (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores))
-
+		(< (get-entity-food @id) 70)
 		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
-		(< (get-entity-food @id) 30)
-		
 		(simulate-move @cell1
 			(get-entity-coordinates @id)
 			(get-entity-coordinates @id1)
@@ -39,51 +19,35 @@
 		(move-entity-to @id @cell1 :diagonal)
 		(feed-entity @id @id1))
 
-
-(defrule get-foot-alone
-	:group
-		:herbivores
-	:when
-    (not (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores)))
-
-		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
-		
-		(simulate-move @cell1
-			(get-entity-coordinates @id)
-			(get-entity-coordinates @id1)
-			:diagonal)
-	:do
-		(move-entity-to @id @cell1 :diagonal)
-		(feed-entity @id @id1))
-
-
+; drink
 (defrule drink
 	:group
 		:herbivores
 	:when
-		(< (get-entity-water @id) 50)
+		(< (get-entity-water @id) 40)
 		(search-cell @cell1 (area-around @cell1 :water))
 	:do
 		(move-entity-to @id @cell1 :diagonal)
 		(drink-water @id))
 
+
+(defrule search-food
+	:group :herbivores
+	:when
+		(> (get-entity-movements @id) 1)
+		(search-distant-cell @cell1 (equal (get-cell-type @cell1) :desert))
+		(simulate-move @cell2 (get-entity-coordinates @id) @cell1 :diagonal)
+	:do
+		(move-entity-to @id @cell2 :diagonal))
+
 ; Find water
 (defrule search-water
-  :group 
+  :group
     :herbivores
   :when
+		(< (get-entity-water @id) 60)
+
     (not (search-cell-lim @cell1 3
       (equal (get-cell-type @cell1) :water)))
   :do 
     (move-entity @id :north 3))
-
-
-; Find food
-(defrule search-food
-  :group 
-    :herbivores
-  :when
-		(not (view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id))))
-      
-  :do 
-    (move-entity-to @id @cell1 :diagonal))
--- IPN/AI/MicroMundo/test.lisp
+++ IPN/AI/MicroMundo/test.lisp
@@ -7,32 +7,10 @@
 	  (set-entities :herbivore 10 :desert))
 
 
-; Find food
 (defrule search-food
-  :group 
-    :herbivores
-  :when
-		(< (get-entity-food @id) 50)
-
-    (search-distant-cell @cell1
-      (equal (get-cell-type @cell1) :desert)
-      (simulate-move @cell1 (get-entity-coordinates @id) @cell1 :diagonal))
-  :do 
-    (move-entity-to @id @cell1 :diagonal))
-
-
-
-(defrule get-foot
-	:group
-		:herbivores
+	:group :herbivores
 	:when
-    
-		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
-		
-		(simulate-move @cell1
-			(get-entity-coordinates @id)
-			(get-entity-coordinates @id1)
-			:diagonal)
+		(> (get-entity-movements @id) 1)
+		(search-distant-cell @cell1 (equal (get-cell-type @cell1) :grass))
 	:do
-		(move-entity-to @id @cell1 :diagonal)
-		(feed-entity @id @id1))
+		(move-entity-to @id @cell1 :diagonal))
