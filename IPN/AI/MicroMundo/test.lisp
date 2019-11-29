83820b2949bae90e2126225da39603e816ff024f
SoyOscarRH <soyoscarrh@gmail.com>
2019-11-29 03:46:23 -0600

    work (~4)

--- IPN/AI/MicroMundo/test.lisp
+++ IPN/AI/MicroMundo/test.lisp
@@ -4,54 +4,74 @@
 	:when
 	:do (set-entities :herbivore 10 :desert))
 
-; ==== Eat stuff ====
-(defrule feed-alone
-	:group :herbivores
-	:when
-    (not (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores)))
-    (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores))
-		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
-		(simulate-move @cell1
-			(get-entity-coordinates @id)
-			(get-entity-coordinates @id1)
-			:orthogonal)
-	:do
-		(move-entity-to @id @cell1 :orthogonal)
-		(feed-entity @id @id1))
-
-
-(defrule feed-group
-	:group :herbivores
-	:when
-    (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores))
-    (< (get-entity-water @id) 40)
-		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
-		(simulate-move @cell1
-			(get-entity-coordinates @id)
-			(get-entity-coordinates @id1)
-			:orthogonal)
-	:do
-		(move-entity-to @id @cell1 :orthogonal)
-		(feed-entity @id @id1))
-
 ; ==== Move around to find food ====
 (defrule move
 	:group :herbivores
 	:when
+    (< (get-entity-food @id) 60)
+
 		(search-distant-cell @cell1
+      (< (get-cell-entity-type-number @cell1 :herbivore) 1)
+      (equal :desert (get-cell-type @cell1))
+      (search-cell@cell1 <conditions>)
+      (not (area-around @cell1 :water)
+      )
+		  (simulate-move @cell2 (get-entity-coordinates @id) @cell1 :orthogonal))
+	:do
+		(move-entity-to @id @cell1 :orthogonal))
+
+(defrule move-des
+	:group :herbivores
+	:when
+    (< (get-entity-food @id) 30)
+
+		(search-distant-cell @cell1
+      (< (get-cell-entity-type-number @cell1 :herbivore) 1)
       (equal :desert (get-cell-type @cell1))
       (not (area-around @cell1 :water))
 		  (simulate-move @cell2 (get-entity-coordinates @id) @cell1 :orthogonal))
 	:do
 		(move-entity-to @id @cell1 :orthogonal))
 
+; ==== Eat stuff ====
+(defrule feed-alone
+	:group :herbivores
+	:when
+    (equal 1 (get-entity-type-count @id :herbivore))
+
+		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
+		(simulate-move @cell1
+			(get-entity-coordinates @id)
+			(get-entity-coordinates @id1)
+			:orthogonal)
+	:do
+		(move-entity-to @id (get-entity-coordinates @id1) :orthogonal)
+		(feed-entity @id @id1))
+
+(defrule feed-group
+	:group :herbivores
+	:when
+    (< 1 (get-entity-type-count @id :herbivore))
+    (< (get-entity-food @id) 50)
+		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
+		(simulate-move @cell1
+			(get-entity-coordinates @id)
+			(get-entity-coordinates @id1)
+			:orthogonal)
+	:do
+		(move-entity-to @id @cell1 :orthogonal)
+		(feed-entity @id @id1))
+
+
 
 ; ==== Drink stuff ====
 (defrule drink
 	:group :herbivores
 	:when
-    (< (get-entity-water @id) 50)
-		(search-cell @cell1 (area-around @cell1 :water))
+    (< (get-entity-water @id) 40)
+		(search-cell @cell1 
+      (area-around @cell1 :water)
+      (not (equal :contamination (get-cell-type @cell1))))
 	:do
 		(move-entity-to @id @cell1 :diagonal)
 		(drink-water @id))
@@ -61,7 +81,7 @@
 (defrule search-water
   :group :herbivores
   :when
-    (< (get-entity-water @id) 50)
+    (< (get-entity-water @id) 40)
     (not (search-cell @cell1 (equal (get-cell-type @cell1) :water)))
   :do 
-    (move-entity @id :north 3))
+    (move-entity @id :south 3))
