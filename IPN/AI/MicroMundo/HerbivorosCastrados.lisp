0fe99c2dac948704d7b412fd58eca3da05820470
SoyOscarRH <soyoscarrh@gmail.com>
2019-11-29 02:03:11 -0600

    a

--- IPN/AI/MicroMundo/HerbivorosCastrados.lisp
+++ IPN/AI/MicroMundo/HerbivorosCastrados.lisp
@@ -5,9 +5,11 @@
 	:do (set-entities :herbivore 10 :desert))
 
 ; ==== Eat stuff ====
-(defrule feed
+(defrule feed-alone
 	:group :herbivores
 	:when
+    (not (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores)))
+    (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores))
 		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
 		(simulate-move @cell1
 			(get-entity-coordinates @id)
@@ -17,31 +19,49 @@
 		(move-entity-to @id @cell1 :orthogonal)
 		(feed-entity @id @id1))
 
-; ==== Drink stuff ====
-(defrule drink
+
+(defrule feed-group
 	:group :herbivores
 	:when
-		(search-cell @cell1 (area-around @cell1 :water))
+    (view-field-vision @id2 (equal (get-entity-type @id2) :herbivores))
+    (< (get-entity-water @id) 40)
+		(view-field-vision @id1 (in (get-entity-type @id1) (get-consumable-type @id)))
+		(simulate-move @cell1
+			(get-entity-coordinates @id)
+			(get-entity-coordinates @id1)
+			:orthogonal)
 	:do
-		(move-entity-to @id @cell1 :diagonal)
-		(drink-water @id))
+		(move-entity-to @id @cell1 :orthogonal)
+		(feed-entity @id @id1))
 
 ; ==== Move around to find food ====
-(defrule search-food
+(defrule move
 	:group :herbivores
 	:when
 		(search-distant-cell @cell1
       (equal :desert (get-cell-type @cell1))
+      (not (area-around @cell1 :water))
 		  (simulate-move @cell2 (get-entity-coordinates @id) @cell1 :orthogonal))
 	:do
 		(move-entity-to @id @cell1 :orthogonal))
+
+
+; ==== Drink stuff ====
+(defrule drink
+	:group :herbivores
+	:when
+    (< (get-entity-water @id) 50)
+		(search-cell @cell1 (area-around @cell1 :water))
+	:do
+		(move-entity-to @id @cell1 :diagonal)
+		(drink-water @id))
 
 ; ==== Move around to find water ====
 ; Find water
 (defrule search-water
   :group :herbivores
   :when
-    (not (search-cell @cell1
-      (equal (get-cell-type @cell1) :water)))
+    (< (get-entity-water @id) 50)
+    (not (search-cell @cell1 (equal (get-cell-type @cell1) :water)))
   :do 
-    (move-entity @id :south 3))
+    (move-entity @id :north 3))
