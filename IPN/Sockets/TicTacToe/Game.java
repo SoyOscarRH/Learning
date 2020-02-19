6baf3eaf7098f5e0a10881c74591af4463eee619
SoyOscarRH <soyoscarrh@gmail.com>
2020-02-19 07:43:14 -0600

    Add base

--- IPN/Sockets/TicTacToe/Game.java
+++ IPN/Sockets/TicTacToe/Game.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Game {
   public static void main(String args[]) {
-    final var plays = JOptionPane.showInputDialog(null, "What's your name?");
+    final var plays = JOptionPane.showInputDialog(null, "How many games?");
     for (var i = 0; i < Integer.valueOf(plays); ++i) {
       new Thread(new TicTacToe()).start();
     }
