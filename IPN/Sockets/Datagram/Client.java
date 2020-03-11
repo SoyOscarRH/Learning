4cc2a25bd5441b0f27fe1f3d35a7d8b20fdb32ba
SoyOscarRH <soyoscarrh@gmail.com>
2020-03-11 08:58:51 -0600

    Add Datagram

--- IPN/Sockets/Datagram/Client.java
+++ IPN/Sockets/Datagram/Client.java
@@ -3,17 +3,24 @@ import java.net.*;
 
 public class Client {
   static final String ip = "127.0.0.1";
-  static final int port = 2000;
+  static final int myPort = 2001;
+  static final int otherPort = 2000;
 
   public static void main(String[] args) {
-    try (DatagramSocket socket = new DatagramSocket()) {
+    try (DatagramSocket socket = new DatagramSocket(2001)) {
       final var reader = new BufferedReader(new InputStreamReader(System.in));
-      final var message = reader.readLine();
+      var message = reader.readLine();
       byte[] data = message.getBytes();
 
       final var ip_name = InetAddress.getByName(ip);
-      final var packet = new DatagramPacket(data, data.length, ip_name, port);
+      final var packet = new DatagramPacket(data, data.length, ip_name, otherPort);
       socket.send(packet);
+
+      final var inPacket = new DatagramPacket(new byte[2000], 2000);
+      socket.receive(inPacket);
+      message = new String(inPacket.getData(), 0, inPacket.getLength());
+      System.out.println("With message: " + message);
+
     } catch (Exception e) {
       e.printStackTrace();
     }
--- IPN/Sockets/Datagram/Server.java
+++ IPN/Sockets/Datagram/Server.java
@@ -2,20 +2,24 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
+  static final String ip = "127.0.0.1";
+  static final int myPort = 2000;
+  static final int otherPort = 2001;
+
   public static void main(String[] args) {
-    try (final var socket = new DatagramSocket(2000)) {
+    try (final var socket = new DatagramSocket(myPort)) {
       while (true) {
-        var packet = new DatagramPacket(new byte[2000], 2000);
-        socket.receive(packet);
-        System.out.println("Datagram info " + packet.getAddress() + ":" + packet.getPort());
-        final var message = new String(packet.getData(), 0, packet.getLength());
+        final var inPacket = new DatagramPacket(new byte[2000], 2000);
+        socket.receive(inPacket);
+        System.out.println("Datagram info " + inPacket.getAddress() + ":" + inPacket.getPort());
+        final var message = new String(inPacket.getData(), 0, inPacket.getLength());
         System.out.println("With message: " + message);
 
         final var result = "echo " + message;
         byte[] data = result.getBytes();
 
-        packet = new DatagramPacket(data, data.length, ip_name, port);
-        socket.send(packet);
+        final var outPacket = new DatagramPacket(data, data.length, ip, otherPort);
+        socket.send(outPacket);
 
       }
     } catch (Exception e) {
