2143560dc91808cfbf15e9c921d43c09d5e56e87
SoyOscarRH <soyoscarrh@gmail.com>
2020-03-20 16:20:52 -0600

    Add split fo messages

--- IPN/Sockets/Datagram/Client.java
+++ IPN/Sockets/Datagram/Client.java
@@ -1,19 +1,29 @@
 import java.io.*;
+import java.lang.Math;
 import java.net.*;
+import java.util.Arrays;
 
 public class Client {
   static final int serverPort = 2000;
   static final int clientPort = 2001;
 
   public static void main(String[] args) {
+    final var reader = new BufferedReader(new InputStreamReader(System.in));
+
     try (final var socket = new DatagramSocket(clientPort)) {
-      final var reader = new BufferedReader(new InputStreamReader(System.in));
+      final var ip = InetAddress.getByName("127.0.0.1");
       final var data = reader.readLine().getBytes();
 
-      final var ip = InetAddress.getByName("127.0.0.1");
-      socket.send(new DatagramPacket(data, data.length, ip, serverPort));
+      final var packets = Math.round(Math.ceil(data.length / 20.0));
+      final var split = Long.toString(packets).getBytes();
+      socket.send(new DatagramPacket(split, split.length, ip, serverPort));
 
-      final var inPacket = new DatagramPacket(new byte[2000], 2000);
+      for (var offset = 0; offset < data.length; offset += 20) {
+        final var local_data = Arrays.copyOfRange(data, offset, offset + 20);
+        socket.send(new DatagramPacket(local_data, local_data.length, ip, serverPort));
+      }
+
+      final var inPacket = new DatagramPacket(new byte[20000], 20000);
       socket.receive(inPacket);
       final var message = new String(inPacket.getData(), 0, inPacket.getLength());
       System.out.println(">" + message);
--- IPN/Sockets/Datagram/Server.java
+++ IPN/Sockets/Datagram/Server.java
@@ -8,11 +8,19 @@ public class Server {
   public static void main(String[] args) {
     try (final var socket = new DatagramSocket(serverPort)) {
       while (true) {
-        final var inPacket = new DatagramPacket(new byte[2000], 2000);
+        final var inPacket = new DatagramPacket(new byte[20], 20);
         socket.receive(inPacket);
 
-        final var message = new String(inPacket.getData(), 0, inPacket.getLength());
-        System.out.println(">" + inPacket.getAddress() + ":" + inPacket.getPort());
+        final var packets_raw = new String(inPacket.getData(), 0, inPacket.getLength());
+        final var packets = Integer.parseInt(packets_raw);
+        
+        var message = "";
+        for (int i = 0; i < packets; ++i) {
+          socket.receive(inPacket);
+          message += new String(inPacket.getData(), 0, inPacket.getLength());
+        }
+
+        System.out.println("> Packets " + packets + " from " + inPacket.getAddress() + ":" + inPacket.getPort());
         System.out.println("  " + message);
 
         final var data = ("echo " + message).getBytes();
