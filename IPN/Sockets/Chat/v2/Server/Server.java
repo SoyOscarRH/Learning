d61c884910a79394617310069184584c979483e7
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-20 15:11:06 -0500

    Probably init

---
+++ IPN/Sockets/Chat/v2/Server/Server.java
@@ -0,0 +1,137 @@
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+public class Server {
+  protected static ArrayList<String> onlineUserNames;
+  private static MulticastSocket s;
+  public static InetAddress group;
+  public static int ports, portc;
+  public static String address;
+
+  public static void main(String[] args) {
+    Runnable t = () -> Private.PrivateServerSocket();
+    new Thread(t).start();
+
+    try {
+      onlineUserNames = new ArrayList<String>();
+      System.setProperty("java.net.preferIPv4Stack", "true");
+      ports = 9000;
+      portc = 9001;
+      address = "230.1.1.1";
+      s = new MulticastSocket(ports);
+      System.out.println("\n\tMulticast service initialized...");
+      group = null;
+      try {
+        group = InetAddress.getByName(address);
+      } catch (UnknownHostException e) {
+        System.err.println("\n\tInvalid address.");
+        System.exit(0);
+      } // End try - catch.
+      s.joinGroup(group);
+      s.setTimeToLive(200);
+      while (true) {
+        DatagramPacket p = new DatagramPacket(new byte[1500], 1500);
+        s.receive(p);
+        String msg = new String(p.getData(), 0, p.getLength());
+        System.out.println("\n\tMessage received from: " + p.getAddress() + " : " + p.getPort()
+            + "\n\tMessage: " + msg);
+        Type(msg);
+        try {
+          Thread.sleep(5000);
+        } catch (InterruptedException ie) {
+        } // End try - catch.
+      } // End for.
+    } catch (Exception e) {
+      e.printStackTrace();
+    } // End try - catch.
+
+  } // End main.
+
+  /* The type of the message received from the client
+   * can be <msg>, <init> or <private>. If it's <msg>, the
+   * server will send the message to the common chat window,
+   * <init> if a new user wants to join to the conversation,
+   * and <private> to open a personal chat with another user.
+   */
+
+  public static void Type(String msg) throws IOException {
+    String[] sp = msg.split(" ");
+    msg = "";
+
+    if (sp[0].equalsIgnoreCase("<init>")) {
+      String type = "<init>";
+      String userName = "";
+      byte[] b = type.getBytes();
+      byte[] b1;
+      String user;
+      DatagramPacket p = new DatagramPacket(b, b.length, group, portc);
+      s.send(p);
+      for (int i = 1; i < sp.length; i++) {
+        userName = userName + sp[i] + " ";
+      } // End for.
+      onlineUserNames.add(userName);
+      sendNumberOfOnlineUsersToClient();
+      for (int i = 0; i < onlineUserNames.size(); i++) {
+        user = onlineUserNames.get(i);
+        b1 = user.getBytes();
+        DatagramPacket p1 = new DatagramPacket(b1, b1.length, group, portc);
+        s.send(p1);
+      } // End for.
+    } // End if.
+
+    if (sp[0].equalsIgnoreCase("<msg>")) {
+      String type = "<msg>";
+      String aux = "";
+      byte[] b1 = type.getBytes();
+      DatagramPacket p1 = new DatagramPacket(b1, b1.length, group, portc);
+      s.send(p1);
+      for (int i = 1; i < sp.length; i++) {
+        msg = msg + sp[i] + " ";
+      } // End for.
+      aux = Emotion.replaceEmotions(msg);
+      msg = aux;
+      byte[] b = msg.getBytes();
+      DatagramPacket p = new DatagramPacket(b, b.length, group, portc);
+      s.send(p);
+    } // End if.
+
+    if (sp[0].equalsIgnoreCase("<private>")) {
+      String msgFrom = sp[sp.length - 1];
+      String msgFor = "";
+      for (int i = 1; i < sp.length - 2; i++) {
+        msgFor = msgFor + sp[i];
+      } // End for.
+      System.out.println("\n\tPrivate Message for: " + msgFor + ". From: " + msgFrom + ".");
+      String type = "<private>";
+      byte[] b = type.getBytes();
+      DatagramPacket p = new DatagramPacket(b, b.length, group, portc);
+      s.send(p);
+      b = msgFrom.getBytes();
+      DatagramPacket p1 = new DatagramPacket(b, b.length, group, portc);
+      s.send(p1);
+      b = msgFor.getBytes();
+      DatagramPacket p2 = new DatagramPacket(b, b.length, group, portc);
+      s.send(p2);
+    } // End if.
+
+  } // End Type.
+
+  public static void sendNumberOfOnlineUsersToClient() throws IOException {
+    try (final var byteStream = new ByteArrayOutputStream()) {
+      try (final var stream = new DataOutputStream(byteStream)) {
+        stream.writeInt(onlineUserNames.size());
+        stream.flush();
+
+        final var data = byteStream.toByteArray();
+        s.send(new DatagramPacket(data, data.length, group, portc));
+      }
+    }
+  }
+}
