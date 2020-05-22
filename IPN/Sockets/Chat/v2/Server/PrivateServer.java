509883fe85951004adabb4932893448bf0b8f983
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-22 18:40:55 -0500

    Update PrivateServer.java

--- IPN/Sockets/Chat/v2/Server/PrivateServer.java
+++ IPN/Sockets/Chat/v2/Server/PrivateServer.java
@@ -5,14 +5,9 @@ import java.net.InetAddress;
 import java.util.Hashtable;
 
 class PrivateServer {
-  static Hashtable<String, Integer> host_to_port;
-  private static DatagramSocket server1;
-
   public static void startServer() {
-    host_to_port = new Hashtable<String, Integer>();
-
-    try {
-      server1 = new DatagramSocket(9709);
+    final var host_to_port = new Hashtable<String, Integer>();
+    try (final var server1 = new DatagramSocket(9709)) {
       System.out.println("Private server is online");
 
       while (true) {
@@ -23,21 +18,19 @@ class PrivateServer {
         System.out.printf("private message from: %s:%s\n", packet.getAddress(), packet.getPort());
         System.out.printf("\tdata: %s\n\n", message);
 
-        String[] server = message.split(" ");
-        final var message_type = server[0].toLowerCase();
-
+        String[] message_part = message.split(" ");
+        final var message_type = message_part[0].toLowerCase();
         message = "";
 
         if (message_type.equals("<init>")) {
-          for (int i = 1; i < server.length; i++) message += server[i];
+          for (int i = 1; i < message_part.length; i++) message += message_part[i];
           host_to_port.put(message, packet.getPort());
         }
 
         if (message_type.equals("<msg>")) {
-          for (int i = 2; i < server.length; i++) message += server[i] + " ";
-          final var user = server[1];
-          message = Emotion.replaceEmotions(message);
-          byte[] raw = message.getBytes();
+          for (int i = 2; i < message_part.length; i++) message += message_part[i] + " ";
+          final var user = message_part[1];
+          byte[] raw = Emotion.replaceEmotions(message).getBytes();
           final var address = InetAddress.getByName("127.0.0.1");
 
           server1.send(new DatagramPacket(raw, raw.length, packet.getAddress(), packet.getPort()));
