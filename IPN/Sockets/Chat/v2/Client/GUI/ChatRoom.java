ce6d2d62a23f031b09d0140a661879d41f8d55be
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-23 20:23:10 -0500

    Update ChatRoom.java

--- IPN/Sockets/Chat/v2/Client/GUI/ChatRoom.java
+++ IPN/Sockets/Chat/v2/Client/GUI/ChatRoom.java
@@ -4,8 +4,6 @@ import Main.Main;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.ByteArrayInputStream;
@@ -26,133 +24,98 @@ import javax.swing.JTextField;
 public class ChatRoom {
   String username;
   String privateMsg;
-  String aux = "";
+  String messages = "";
 
   public ChatRoom(final String s) {
-
-    final var dlm = new DefaultListModel<String>();
-    final var onlineUsers = new JList<String>(dlm);
-    final var aList = new ArrayList<String>();
-    final var ep = new JEditorPane();
-
+    final var onlineUsers = new JList<String>();
+    final var messageSection = new JEditorPane();
 
     new Thread(() -> {
-      for (;;) {
-        /* Receive the type of the datagram packet; <msg>, <init> or <private>,
-         * depending the message there is an if sentence for any case.
-         */
-        String type = "";
+      while (true) {
         try {
-          final DatagramPacket p = new DatagramPacket(new byte[1024], 1024);
-          Main.cl.receive(p);
-          type = new String(p.getData(), 0, p.getLength());
-          System.out.printf("== %s ===\n", type);
+          final var packet = new DatagramPacket(new byte[1024], 1024);
+          Main.cl.receive(packet);
+          final var type = new String(packet.getData(), 0, packet.getLength()).toLowerCase();
 
+          if (type.equals("<init>")) {
+            final var users = new DefaultListModel<String>();
+
+            final DatagramPacket p = new DatagramPacket(new byte[1024], 1024);
+            Main.cl.receive(p);
+
+            final var bytes = new ByteArrayInputStream(p.getData());
+            final var numUsers = new DataInputStream(bytes).readInt();
+
+            for (var i = 0; i < numUsers; ++i) {
+              final var packetName = new DatagramPacket(new byte[1024], 1024);
+              Main.cl.receive(packetName);
+              final var user = new String(packetName.getData(), 0, packetName.getLength());
+              users.addElement(user);
+            }
+            
+            onlineUsers.setModel(users);
+            onlineUsers.updateUI();
+          }
+
+          if (type.equals("<msg>")) {
+            final var packetMessage = new DatagramPacket(new byte[1024], 1024);
+            Main.cl.receive(packetMessage);
+            final var message = new String(packetMessage.getData(), 0, packetMessage.getLength());
+
+            System.out.printf(
+                "message from: %s:%s\n", packetMessage.getAddress(), packetMessage.getPort());
+            System.out.printf("\tmessage: %s\n", message);
+
+            messages += message + "<br />";
+            messageSection.setText(messages);
+          }
+
+          if (type.equals("<private>")) {
+            final var packetFrom = new DatagramPacket(new byte[1024], 1024);
+            Main.cl.receive(packetFrom);
+            final var msgFrom = new String(packetFrom.getData(), 0, packetFrom.getLength());
+
+            final var packetFor = new DatagramPacket(new byte[1024], 1024);
+            Main.cl.receive(packetFor);
+            final var msgFor = new String(packetFor.getData(), 0, packetFor.getLength());
+
+            System.out.printf("private for: %s from %s", msgFor, msgFrom);
+
+            if (msgFor.equals(username)) {
+              final var address = InetAddress.getByName(Private.host);
+
+              final var privateSession = new Private();
+              final var raw = ("<init> <" + username + ">").getBytes();
+              Private.cl.send(new DatagramPacket(raw, raw.length, address, Private.ports));
+              privateSession.Components(username, msgFrom);
+            }
+          }
         } catch (final IOException e) {
           e.printStackTrace();
-        } // End try - catch.
-
-        if (type.equalsIgnoreCase("<init>")) {
-          dlm.clear();
-          aList.clear();
-          try {
-            int numUsers = 0;
-            final DatagramPacket p = new DatagramPacket(new byte[1500], 1500);
-            Main.cl.receive(p);
-            final ByteArrayInputStream bais = new ByteArrayInputStream(p.getData());
-            final DataInputStream dis = new DataInputStream(bais);
-            numUsers = (int) dis.readInt();
-
-            System.out.printf("== %d ===\n", numUsers);
-            String user = "";
-            for (int i = 0; i < numUsers; i++) {
-              final DatagramPacket p2 = new DatagramPacket(new byte[1024], 1024);
-              Main.cl.receive(p2);
-              user = new String(p2.getData(), 0, p2.getLength());
-              System.out.printf("== %s ===\n", user);
-              aList.add(user);
-              dlm.addElement(aList.get(i));
-            } // End for.
-
-            onlineUsers.setModel(dlm);
-            onlineUsers.updateUI();
-
-          } catch (final IOException e) {
-            e.printStackTrace();
-          } // End try - catch.
-
-        } // End if.
-
-        if (type.equalsIgnoreCase("<msg>")) {
-          String msg = "";
-          try {
-            final DatagramPacket p = new DatagramPacket(new byte[1500], 1500);
-            Main.cl.receive(p);
-            msg = new String(p.getData(), 0, p.getLength());
-            System.out.println("\n\tMessage received from: " + p.getAddress() + " : " + p.getPort()
-                + "\n\tMessage: " + msg);
-          } catch (final IOException e) {
-            e.printStackTrace();
-          } // End try - catch.
-          aux = aux + msg + "<BR>";
-          ep.setText(aux);
-        } // End if.
-
-        if (type.equalsIgnoreCase("<private>")) {
-          DatagramPacket p = new DatagramPacket(new byte[1500], 1500);
-          String msgFrom = "";
-          String msgFor = "";
-          try {
-            Main.cl.receive(p);
-            msgFrom = new String(p.getData(), 0, p.getLength());
-            p = new DatagramPacket(new byte[1500], 1500);
-            Main.cl.receive(p);
-            msgFor = new String(p.getData(), 0, p.getLength());
-          } catch (final IOException e) {
-            e.printStackTrace();
-          } // End try - catch.
-          System.out.println("\n\tPrivate Message for: " + msgFor + ". From: " + msgFrom + ".");
-          if (msgFor.equalsIgnoreCase(username)) {
-            try {
-              final Private pmsg = new Private();
-              final String s2 = "<init> <" + username + ">";
-              final byte[] b = s2.getBytes();
-              p = new DatagramPacket(
-                  b, b.length, InetAddress.getByName(Private.host), Private.ports);
-              Private.cl.send(p);
-              pmsg.Components(username, msgFrom);
-
-            } catch (final Exception e) {
-              e.printStackTrace();
-            } // End try - catch.
-          } // End if.
-        } // End if.
-
-      } // End forever.
+        }
+      }
     }).start();
 
     final var frame = new JFrame("TeamWork-Chat: " + s);
 
     frame.getContentPane().setBackground(Color.white);
 
-
     final var label = new JLabel("Online Users:");
 
     final var tf = new JTextField();
     final var logo = new JLabel();
-    final var scroller1 = new JScrollPane(
-        onlineUsers, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-    final var scroller = new JScrollPane(
-        ep, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+    final var verticalYes = JScrollPane.VERTICAL_SCROLLBAR_ALWAYS;
+    final var horizontalNo = JScrollPane.HORIZONTAL_SCROLLBAR_NEVER;
+
+    final var scroller1 = new JScrollPane(onlineUsers, verticalYes, horizontalNo);
+    final var scroller = new JScrollPane(messageSection, verticalYes, horizontalNo);
+
     final var un = s + ":";
     username = s;
 
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLocationRelativeTo(null);
-    frame.setResizable(false);
-    frame.setSize(400, 400);
-    frame.setVisible(true);
     frame.setLayout(null);
+    frame.setSize(400, 400);
 
     final ImageIcon ic = new ImageIcon("Logo.png");
     final Image im = ic.getImage();
@@ -168,14 +131,13 @@ public class ChatRoom {
 
     onlineUsers.setForeground(Color.LIGHT_GRAY);
     tf.setForeground(Color.blue);
-    ep.setForeground(Color.blue);
+    messageSection.setForeground(Color.blue);
 
-    onlineUsers.setFont(new Font("Times New Roman", Font.BOLD, 12));
-    tf.setFont(new Font("Times New Roman", Font.BOLD, 12));
-    ep.setFont(new Font("Times New Roman", Font.BOLD, 12));
-    label.setFont(new Font("Helvetica", Font.BOLD, 14));
+    onlineUsers.setFont(new Font("Times New Roman", Font.BOLD, 14));
+    tf.setFont(new Font("Times New Roman", Font.BOLD, 14));
+    messageSection.setFont(new Font("Times New Roman", Font.BOLD, 15));
+    label.setFont(new Font("Helvetica", Font.BOLD, 15));
 
-    /* Mouse event listener. */
     onlineUsers.addMouseListener(new MouseAdapter() {
       public void mouseClicked(final MouseEvent event) {
         if (event.getClickCount() != 2)
@@ -195,9 +157,8 @@ public class ChatRoom {
       }
     });
 
-    ep.setContentType("text/html");
-    onlineUsers.setModel(dlm);
-    ep.setEditable(false);
+    messageSection.setContentType("text/html");
+    messageSection.setEditable(false);
 
     frame.add(scroller1);
     frame.add(scroller);
@@ -205,11 +166,16 @@ public class ChatRoom {
     frame.add(logo);
     frame.add(tf);
 
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setVisible(true);
+
     tf.addActionListener(e -> {
       try {
         final var newMessage = "<msg> " + un + " " + tf.getText();
         final var raw = newMessage.getBytes();
-        final DatagramPacket p = new DatagramPacket(raw, raw.length, Main.group, Main.ports);
+        final var p = new DatagramPacket(raw, raw.length, Main.group, Main.ports);
         Main.cl.send(p);
         tf.setText("");
       } catch (final Exception e1) {
@@ -231,13 +197,11 @@ public class ChatRoom {
     byte[] b = privateMsg.getBytes();
     DatagramPacket p = new DatagramPacket(b, b.length, Main.group, Main.ports);
     Main.cl.send(p);
-    final Private pmsg = new Private();
-    final String s = "<init> <" + username + ">";
+    final var privateSession = new Private();
+    final var s = "<init> <" + username + ">";
     b = s.getBytes();
     p = new DatagramPacket(b, b.length, InetAddress.getByName(Private.host), Private.ports);
     Private.cl.send(p);
-    pmsg.Components(username, msgFor);
-
-  } // End PrivateMessage.
-
-} // End class.
+    privateSession.Components(username, msgFor);
+  }
+}
