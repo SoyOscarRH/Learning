c4a1d334d4d85f7d4b65628154ded15f5d137303
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-28 17:10:39 -0500

    add better code

--- IPN/Sockets/TicTacToeInC/server.cpp
+++ IPN/Sockets/TicTacToeInC/server.cpp
@@ -12,68 +12,56 @@
 #include <unordered_map>
 #include <vector>
 
-#include "./error.h"
-
 typedef int id;
 
-const char* PORT = "9034";
-
-// Get sockaddr, IPv4 or IPv6:
 void* get_in_addr(struct sockaddr* sa) {
   if (sa->sa_family == AF_INET) { return &(((struct sockaddr_in*)sa)->sin_addr); }
 
   return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-id get_listener_socket() {
-  int listener;  // Listening socket descriptor
-  int rv;
+id get_listener_socket(const char* port_name) {
+  struct addrinfo* possible_addresses;
+  struct addrinfo* hints = (struct addrinfo*)calloc(1, sizeof(struct addrinfo));
+  hints->ai_family = AF_UNSPEC;
+  hints->ai_socktype = SOCK_STREAM;
+  hints->ai_flags = AI_PASSIVE;
 
-  struct addrinfo hints, *ai, *p;
-
-  // Get us a socket and bind it
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-  if ((rv = getaddrinfo(NULL, PORT, &hints, &ai)) != 0) {
-    fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+  const int error_code = getaddrinfo(NULL, port_name, hints, &possible_addresses);
+  if (error_code != 0) {
+    fprintf(stderr, "error setting up: %s\n", gai_strerror(error_code));
     exit(1);
   }
 
-  for (p = ai; p != NULL; p = p->ai_next) {
-    listener = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-    if (listener < 0) { continue; }
+  id listener;
+  int valid_option = 0;
+  for (struct addrinfo* option = possible_addresses; option and !valid_option; option = option->ai_next) {
+    listener = socket(option->ai_family, option->ai_socktype, option->ai_protocol);
+    if (listener < 0) continue;
 
-    // Lose the pesky "address already in use" error message
-    int yes = 1;  // For setsockopt() SO_REUSEADDR, below
+    int yes = 1;
     setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-    if (bind(listener, p->ai_addr, p->ai_addrlen) < 0) {
-      close(listener);
-      continue;
-    }
-
-    break;
+    if (bind(listener, option->ai_addr, option->ai_addrlen) != 0) close(listener);
+    else valid_option = true;
   }
 
-  // If we got here, it means we didn't get bound
-  if (p == NULL) return -1;
+  freeaddrinfo(possible_addresses);
+  if (not valid_option) {
+    fprintf(stderr, "Error: I did not find any available ports\n");
+    exit(1);
+  }
 
-  freeaddrinfo(ai);  // All done with this
-
-  // Listen
-  if (listen(listener, 10) == -1) { return -1; }
-
-  if (listener == -1) {
-    fprintf(stderr, "error getting listening socket\n");
+  const int possible_incomming_connections = 10;
+  if (listen(listener, possible_incomming_connections) == -1) {
+    fprintf(stderr, "Error setting up a listen\n");
     exit(1);
   }
 
   return listener;
 }
 
-struct pollfd create_poll_data(id fd) {
+struct pollfd create_poll_data(const id fd) {
   struct pollfd poll_struct;
   poll_struct.fd = fd;
   poll_struct.events = POLLIN;
@@ -130,7 +118,7 @@ void handle_connection(std::vector<struct pollfd>& to_monitor, size_t& i, const 
 }
 
 int main() {
-  const id listener = get_listener_socket();
+  const id listener = get_listener_socket("9034");
   std::vector<struct pollfd> to_monitor {create_poll_data(listener)};
 
   while (true) {
