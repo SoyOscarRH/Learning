3e0385c400e724036cbc33c188b6d125ac26bd5d
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-28 20:38:09 -0500

    A big move, now is tictactoe

--- IPN/Sockets/TicTacToeInC/server.cpp
+++ IPN/Sockets/TicTacToeInC/server.cpp
@@ -8,14 +8,24 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <iostream>
+#include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 typedef int id;
 
+struct game {
+  static const char BLANK = ' ', PLAYER1 = 'O', PLAYER2 = 'X';
+
+  char board[3][3] = {{BLANK, BLANK, BLANK}, {BLANK, BLANK, BLANK}, {BLANK, BLANK, BLANK}};
+  id players[2];
+};
+
 id get_listener_socket(const char* port_name) {
   struct addrinfo* possible_addresses;
-  struct addrinfo* hints = (struct addrinfo*)calloc(1, sizeof(struct addrinfo));
+  struct addrinfo* hints = (struct addrinfo*) calloc(1, sizeof(struct addrinfo));
   hints->ai_family = AF_UNSPEC;
   hints->ai_socktype = SOCK_STREAM;
   hints->ai_flags = AI_PASSIVE;
@@ -65,72 +75,131 @@ struct pollfd create_poll_data(const id fd) {
 
 void show_connection_details(struct sockaddr_storage* address, const id new_client) {
   int family = address->ss_family;
-  void* real_address = family == AF_INET ? (void*)&(((struct sockaddr_in*)address)->sin_addr)
-                                         : (void*)&(((struct sockaddr_in6*)address)->sin6_addr);
+  void* real_address = family == AF_INET ? (void*) &(((struct sockaddr_in*) address)->sin_addr)
+                                         : (void*) &(((struct sockaddr_in6*) address)->sin6_addr);
 
   char remoteIP[INET6_ADDRSTRLEN];
   const char* address_name = inet_ntop(family, real_address, remoteIP, INET6_ADDRSTRLEN);
   printf("New connection from %s is now on socket %d\n", address_name, new_client);
 }
 
-void add_new_connection(std::vector<struct pollfd>& to_monitor, const id listener) {
-  struct sockaddr_storage client_address;
-  socklen_t addrlen = sizeof(client_address);
-  const id new_client = accept(listener, (struct sockaddr*)&client_address, &addrlen);
+int main() {
+  const char* port = "9034";
+  const char hello_message[] = "Welcome to tictactoe. You are now in the queue to find a match\n";
 
-  if (new_client == -1) {
-    perror("error accepting connection");
-    return;
-  }
+  const id listener = get_listener_socket(port);
+  std::vector<pollfd> to_monitor {create_poll_data(listener)};
+  std::unordered_set<id> waiting_for_parther;
+  std::unordered_map<id, game*> games_being_play;
+  printf("Server (port: %s) ready :D\n\n", port);
 
-  show_connection_details(&client_address, new_client);
-  to_monitor.push_back(create_poll_data(new_client));
-}
+  const auto start_possible_games = [&]() {
+    while (waiting_for_parther.size() > 1) {
+      auto queue = begin(waiting_for_parther);
+      id player1 = *queue;
+      queue = waiting_for_parther.erase(queue);
+      id player2 = *queue;
+      queue = waiting_for_parther.erase(queue);
 
-void handle_connection(std::vector<struct pollfd>& to_monitor, size_t& i, const id listener) {
-  char buffer[1024];
-  const id sender = to_monitor[i].fd;
-  const ssize_t num_bytes_read = recv(sender, buffer, sizeof(buffer), 0);
+      struct game* new_game = (struct game*) malloc(sizeof(game));
+      new_game->players[0] = player1, new_game->players[1] = player2;
+      games_being_play[player1] = new_game;
+      games_being_play[player2] = new_game;
 
-  printf("Sending message to the chat: %s", buffer);
+      printf("New game started: %d vs %d", player1, player2);
+      const char message[] = "Player found, Match started\n";
+      if (send(player1, message, sizeof(message), 0) != sizeof(message))
+        fprintf(stderr, "error sending greeting info to player 1\n");
+      if (send(player2, message, sizeof(message), 0) != sizeof(message))
+        fprintf(stderr, "error sending greeting info to player 2\n");
+    }
+  };
 
-  if (num_bytes_read <= 0) {
-    if (num_bytes_read == 0) fprintf(stderr, "socket %d hung up, closing it\n", sender);
-    if (num_bytes_read < 0) fprintf(stderr, "error reading %d, closing it\n", sender);
+  const auto add_new_client = [&]() {
+    struct sockaddr_storage client_address;
+    socklen_t addrlen = sizeof(client_address);
+    const id new_client = accept(listener, (struct sockaddr*) &client_address, &addrlen);
 
+    if (new_client == -1) {
+      perror("error accepting connection");
+      return;
+    }
+
+    if (send(new_client, hello_message, sizeof(hello_message), 0) != sizeof(hello_message))
+      fprintf(stderr, "error sending greeting to %d\n", new_client);
+    else {
+      show_connection_details(&client_address, new_client);
+      to_monitor.push_back(create_poll_data(new_client));
+      waiting_for_parther.insert(new_client);
+    }
+  };
+
+  const auto handle_disconection = [&](const id sender, size_t& i) {
     close(sender);
+    const auto it_waiting = waiting_for_parther.find(sender);
+    if (it_waiting != end(waiting_for_parther)) waiting_for_parther.erase(it_waiting);
+
+    const auto it_game = games_being_play.find(sender);
+    if (it_game != end(games_being_play)) {
+      const game* current_game = it_game->second;
+      const id other =
+          current_game->players[0] == sender ? current_game->players[1] : current_game->players[0];
+
+      const char message[] = "Other player connection is lost. You are waiting for a new match\n";
+      if (send(other, message, sizeof(message), 0) != sizeof(message))
+        fprintf(stderr, "error sending restarting info to %d\n", other);
+      if (send(other, hello_message, sizeof(hello_message), 0) != sizeof(hello_message))
+        fprintf(stderr, "error sending greeting info to %d\n", other);
+
+      waiting_for_parther.insert(other);
+    }
+
     to_monitor[i--] = to_monitor.back();
     to_monitor.pop_back();
+  };
 
-    return;
-  }
+  const auto handle_connection = [&](size_t i) {
+    char buffer[1024];
+    const id sender = to_monitor[i].fd;
+    const ssize_t num_bytes_read = recv(sender, buffer, sizeof(buffer), 0);
 
-  for (auto& client : to_monitor) {
-    const id destination = client.fd;
+    if (num_bytes_read == 2) fprintf(stderr, "empty message from %d\n", sender);
+    else if (num_bytes_read <= 0) {
+      if (num_bytes_read == 0) fprintf(stderr, "socket %d hung up, closing it\n", sender);
+      if (num_bytes_read < 0) fprintf(stderr, "error reading %d, closing it\n", sender);
+      handle_disconection(sender, i);
+    } else {
+      buffer[num_bytes_read - 2] = '\0';
+      printf("Message from %d saying: %s\n", sender, buffer);
+      buffer[num_bytes_read - 2] = '\n';
+      buffer[num_bytes_read - 1] = '\0';
 
-    if (destination == listener) continue;
-    if (destination == sender) continue;
+      const auto it_game = games_being_play.find(sender);
+      if (it_game == end(games_being_play)) return;
 
-    const ssize_t num_bytes_sent = send(destination, buffer, num_bytes_read, 0);
-    if (num_bytes_sent < 1) fprintf(stderr, "error sending to %d\n", destination);
-  }
-}
+      const game* current_game = it_game->second;
+      const id destination =
+          current_game->players[0] == sender ? current_game->players[1] : current_game->players[0];
 
-int main() {
-  const id listener = get_listener_socket("9034");
-  std::vector<struct pollfd> to_monitor {create_poll_data(listener)};
+      if (send(destination, buffer, num_bytes_read - 1, 0) != num_bytes_read - 1)
+        fprintf(stderr, "error sending to %d\n", destination);
+    }
+  };
 
   while (true) {
-    int events_to_check = poll(to_monitor.data(), to_monitor.size(), -1);
+    const int timeout_ms = 50;
+    int events_to_check = poll(to_monitor.data(), to_monitor.size(), timeout_ms);
     if (events_to_check == -1) perror("Error at polling :/");
 
     for (size_t i = 0; i < to_monitor.size() and events_to_check > 0; ++i) {
       if (to_monitor[i].revents & POLLIN) {
-        if (to_monitor[i].fd == listener) add_new_connection(to_monitor, listener);
-        if (to_monitor[i].fd != listener) handle_connection(to_monitor, i, listener);
+        if (to_monitor[i].fd == listener) add_new_client();
+        if (to_monitor[i].fd != listener) handle_connection(i);
         --events_to_check;
       }
     }
+
+    start_possible_games();
   }
 
   return 0;
