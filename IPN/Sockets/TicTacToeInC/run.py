b2e255c2538f8dcc317f0dfed24631803088ec25
SoyOscarRH <soyoscarrh@gmail.com>
2020-05-29 02:17:06 -0500

    Update run.py

--- IPN/Sockets/TicTacToeInC/run.py
+++ IPN/Sockets/TicTacToeInC/run.py
@@ -6,7 +6,7 @@ name, extension = os.path.splitext(filename)
 
 flags = " ".join([
     "-std=c++17", "-lpthread", "-O2", "-Wall", "-Wextra", "-Wshadow",  "-Wpedantic",
-    "-fsanitize=undefined", "-fsanitize=address"])
+    ])
 
 compile = f"g++ {filename} {flags} -o {name}.out"
 os.system(compile)
