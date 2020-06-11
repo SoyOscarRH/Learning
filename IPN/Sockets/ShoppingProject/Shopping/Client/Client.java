cb8e60f9a8f85d26aa7c7fbbc9a28c679aed8f39
SoyOscarRH <soyoscarrh@gmail.com>
2020-06-11 03:18:55 -0500

    Add window creator

--- IPN/Sockets/ShoppingProject/Shopping/Client/Client.java
+++ IPN/Sockets/ShoppingProject/Shopping/Client/Client.java
@@ -18,15 +18,6 @@ public class Client {
       new Thread(() -> ClientServer.startServer()).start();
       Thread.sleep(100);
       Products.print();
-      Products.products.clear();
-      Products.products.add(new Product("azucar", 3, 2, "azucar.jpg"));
-      Products.products.add(new Product("frijol", 10, 5.5, "frijol.jpg"));
-      Products.products.add(new Product("galletas", 3, 10.2, "galletas.jpeg"));
-      Products.products.add(new Product("jugo", 5, 19, "jugo.jpeg"));
-      Products.products.add(new Product("kiwi", 18, 22.5, "kiwi.jpeg"));
-      Products.products.add(new Product("platano", 8, 34.6, "platano.jpeg"));
-      Products.products.add(new Product("sabritas", 8, 7.5, "sabritas.jpeg"));
-      Products.sendUpdateTo(ClientServer.channel);
       new ClientWindow();
     } catch (Exception e) {
       e.printStackTrace();
